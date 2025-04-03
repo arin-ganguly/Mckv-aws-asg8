eca46ff97380e3800ec5fe452613a7965464a586
Arin Ganguly <146520329+arin-ganguly@users.noreply.github.com>
2025-04-03 11:10:00 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV')
+  res.send('It is Arin Ganguly signing in')
 })
 
 app.get('/courses', function (req, res) {
